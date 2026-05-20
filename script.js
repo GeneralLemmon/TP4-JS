cc8e646ae166992a1cf4bd303cd61610d2f0d0a4
GeneralLemmon <tom.drs09@gmail.com>
2026-05-20 12:21:02 +0200

    Problèmes police résolus

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const pokemonList = document.getElementById('pokemon-list');
+
 fetch('https://pokeapi.co/api/v2/pokemon/')
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const pokemon = data.results;
+    pokemon.forEach(pokemon => {
+      const listItem = document.createElement('li');
+      listItem.textContent = pokemon.name;
+      pokemonList.appendChild(listItem);
+    });
+  })
   .catch(err => console.error("Capture des pokémons impossible", err));
-
-  
