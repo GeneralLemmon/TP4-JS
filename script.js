b44830a713472bc3fa1567b331bb26edb3105a6a
GeneralLemmon <tom.drs09@gmail.com>
2026-05-20 12:29:30 +0200

    Bulbizarre effectué

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const pokemonList = document.getElementById('pokemon-list');
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
+fetch('https://pokeapi.co/api/v2/pokemon/1/')
   .then(res => res.json())
-  .then(data => {
-    const pokemon = data.results;
-    pokemon.forEach(pokemon => {
-      const listItem = document.createElement('li');
-      listItem.textContent = pokemon.name;
-      pokemonList.appendChild(listItem);
+  .then(data => { 
+    const pokemonCard = document.querySelector('.pokemon-card');
+    pokemonCard.querySelector('.pokemon-id').textContent = `#${data.id}`;
+    pokemonCard.querySelector('.pokemon-name').textContent = data.name;
+    const typesContainer = pokemonCard.querySelector('.types-container');
+    data.types.forEach(typeInfo => {
+      const typeBadge = document.createElement('span');
+      typeBadge.classList.add('type-badge');
+      typeBadge.textContent = typeInfo.type.name;
+      typesContainer.appendChild(typeBadge);
     });
   })
-  .catch(err => console.error("Capture des pokémons impossible", err));
+  .catch(err => console.error("Capture du pokémon impossible", err));
